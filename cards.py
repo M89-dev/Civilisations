ce297f910e841d69110a3d84d82d3d473ca417a1
Xanark <92733155+Xanark@users.noreply.github.com>
2022-12-13 20:50:15 +0100

    Update cards.py

--- cards.py
+++ cards.py
@@ -2,4 +2,13 @@ import os
 import json
 import time
 
-class cards(self, class="",passif="",civilisation="",life="",resources="",powers=):
+class cards():
+    def __init__(self, classe=None, passif=True, civilisation=None, life=None, resources=None, powers=None, unique=True):
+        self.classe = classe
+        self.passif = passif
+        self.civilisation= civilisation
+        self.life = life
+        self.resources = resources
+        self.powers = powers
+        self.unique = unique
+        
