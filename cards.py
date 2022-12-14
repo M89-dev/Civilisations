0b5d00d3d3d3e9ff386220ed4ade6555de2a3038
Xanark <92733155+Xanark@users.noreply.github.com>
2022-12-14 16:03:52 +0100

    Update cards.py

--- cards.py
+++ cards.py
@@ -3,22 +3,15 @@ import json
 import time
 
 class cards():
-    def __init__(self, classe="", passif=True, civilisation="", life=0, resources=0, powers=0, unique=True):
-        self.classe = classe
-        self.passif = passif
-        self.civilisation= civilisation
-        self.life = life
-        self.resources = resources
-        self.powers = powers
-        self.unique = unique
+    def __init__(self,lien):
+        self.lien = lien 
 
-def open_cards(self,lien):
-    with open(lien) as json_file:
+
+def open_cards(self):
+    with open(self.lien) as json_file:
         data = json.load(json_file)
-        self.classe = []
-        self.passif = 
-        self.civilisation= 
-        self.life = 
-        self.resources = 
-        self.powers = 
-        self.unique = 
+        print(data)
+
+
+cards1=cards()
+open_cards(cards1)
